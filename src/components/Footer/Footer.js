a21a4d82c588c210554ff1d4c7493cd8431b9a89
rickylukman <rickylukman0606@gmail.com>
2021-01-25 21:29:54 +0700

    uhuy

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { 
+    FooterContainer, 
+    FooterSubscription,
+    FooterSubHeading,
+    FooterSubText
+} from './FooterElements'
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterSubscription>
+                <FooterSubHeading>
+                    Bergabung dengan kita uhuy
+                </FooterSubHeading>
+                <FooterSubText>
+                    ini text uhuy
+                </FooterSubText>
+            </FooterSubscription>            
+        </FooterContainer>
+    );
+}
+
+export default Footer;
