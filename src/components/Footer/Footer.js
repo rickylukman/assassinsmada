b2efaf4719cd94a4d1e5d070c1375e5a1f8fbea7
rickylukman <rickylukman0606@gmail.com>
2021-01-25 21:57:24 +0700

    +footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,62 +1,50 @@
 import React from 'react'
+import {
+    FaInstagram,
+    FaYoutube,
+    FaTwitter
+} from 'react-icons/fa';
 import { 
     FooterContainer, 
-    FooterSubscription,
-    FooterSubHeading,
-    FooterSubText,
-    FooterLinksContainer,
-    FooterLinksWrapper,
-    FooterLinkItems,
-    FooterLinkTitle,
-    FooterLink
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    SocialIcon,
+    WebsiteRights,
+    SocialIcons,
+    SocialIconLink
 } from './FooterElements'
 
 const Footer = () => {
     return (
         <FooterContainer>
-            <FooterSubscription>
-                <FooterSubHeading>
-                    Bergabung dengan kita uhuy
-                </FooterSubHeading>
-                <FooterSubText>
-                    ini text uhuy
-                </FooterSubText>
-            </FooterSubscription>
-            <FooterLinksContainer>
-                <FooterLinksWrapper>
-                    <FooterLinkItems>
-                        <FooterLinkTitle>About Us</FooterLinkTitle>
-                        <FooterLink to='/sign-up'>How it works</FooterLink>
-                        <FooterLink to='/'>Testimonials</FooterLink>
-                        <FooterLink to='/'>Careers</FooterLink>
-                        <FooterLink to='/'>Investors</FooterLink>
-                        <FooterLink to='/'>Terms of Service</FooterLink>
-                    </FooterLinkItems>
-                    <FooterLinkItems>
-                        <FooterLinkTitle>Contact Us</FooterLinkTitle>
-                        <FooterLink to='/'>Contact</FooterLink>
-                        <FooterLink to='/'>Support</FooterLink>
-                        <FooterLink to='/'>Destinations</FooterLink>
-                        <FooterLink to='/'>Sponsorships</FooterLink>
-                    </FooterLinkItems>
-                    </FooterLinksWrapper>
-                    <FooterLinksWrapper>
-                    <FooterLinkItems>
-                        <FooterLinkTitle>Videos</FooterLinkTitle>
-                        <FooterLink to='/'>Submit Video</FooterLink>
-                        <FooterLink to='/'>Ambassadors</FooterLink>
-                        <FooterLink to='/'>Agency</FooterLink>
-                        <FooterLink to='/'>Influencer</FooterLink>
-                    </FooterLinkItems>
-                    <FooterLinkItems>
-                        <FooterLinkTitle>Social Media</FooterLinkTitle>
-                        <FooterLink to='/'>Instagram</FooterLink>
-                        <FooterLink to='/'>Facebook</FooterLink>
-                        <FooterLink to='/'>Youtube</FooterLink>
-                        <FooterLink to='/'>Twitter</FooterLink>
-                    </FooterLinkItems>
-                </FooterLinksWrapper>
-            </FooterLinksContainer>            
+            <SocialMedia>
+                <SocialMediaWrap>
+                    <SocialLogo to='/'>
+                        <SocialIcon />
+                        ASSASSIN
+                    </SocialLogo>
+                    <WebsiteRights>ASSASSIN © 2021</WebsiteRights>
+                    <SocialIcons>
+                        <SocialIconLink href={'//www.instagram.com/assassin_smada'} target='_blank' aria-label='Instagram'>
+                        <FaInstagram />
+                        </SocialIconLink>
+                        <SocialIconLink
+                        href={
+                            '//www.youtube.com/channel/UCNA7ZKVkr4Ttjqf6DPGEHvg'
+                        }
+                        rel='noopener noreferrer'
+                        target='_blank'
+                        aria-label='Youtube'
+                        >
+                        <FaYoutube />
+                        </SocialIconLink>
+                        <SocialIconLink href={'//twitter.com/assasin_smada'} target='_blank' aria-label='Twitter'>
+                        <FaTwitter />
+                        </SocialIconLink>
+                    </SocialIcons>
+                </SocialMediaWrap>
+            </SocialMedia>         
         </FooterContainer>
     );
 }
--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
+import { GiSharpSmile } from "react-icons/gi";
 import { Link } from 'react-router-dom';
 
 export const FooterContainer = styled.div`
   background-color: #fff;
-  padding: 4rem 0 2rem 0;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -33,49 +33,51 @@ export const FooterSubText = styled.p`
   font-size: 20px;
 `;
 
-export const FooterLinksContainer = styled.div`
-  width: 100%;
+export const SocialMedia = styled.section`
   max-width: 1000px;
-  display: flex;
-  justify-content: center;
-  @media screen and (max-width: 820px) {
-    padding-top: 32px;
-  }
+  width: 100%;
 `;
 
-export const FooterLinksWrapper = styled.div`
+export const SocialMediaWrap = styled.div`
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  max-width: 1000px;
+  margin: 40px auto 0 auto;
   @media screen and (max-width: 820px) {
     flex-direction: column;
   }
 `;
 
-export const FooterLinkItems = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin: 16px;
-  text-align: left;
-  width: 160px;
-  box-sizing: border-box;
+export const SocialLogo = styled(Link)`
   color: #101522;
-  @media screen and (max-width: 420px) {
-    margin: 0;
-    padding: 10px;
-    width: 100%;
-  }
-`;
-
-export const FooterLinkTitle = styled.h2`
+  justify-self: start;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 2rem;
+  display: flex;
+  align-items: center;
   margin-bottom: 16px;
 `;
 
-export const FooterLink = styled(Link)`
+export const SocialIcon = styled(GiSharpSmile)`
+  margin-right: 10px;
+`;
+
+export const WebsiteRights = styled.small`
   color: #101522;
-  text-decoration: none;
-  margin-bottom: 0.5rem;
-  &:hover {
-    color: #0467fb;
-    transition: 0.3s ease-out;
-  }
+  margin-bottom: 16px;
+`;
+
+export const SocialIcons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 240px;
+`;
+
+export const SocialIconLink = styled.a`
+  color: #101522;
+  font-size: 24px;
 `;
