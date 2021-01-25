8e936f78f1e2eff66315ab296982ef12df10eab2
rickylukman <rickylukman0606@gmail.com>
2021-01-25 21:38:24 +0700

    +footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,12 @@ import {
     FooterContainer, 
     FooterSubscription,
     FooterSubHeading,
-    FooterSubText
+    FooterSubText,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLink
 } from './FooterElements'
 
 const Footer = () => {
@@ -16,7 +21,42 @@ const Footer = () => {
                 <FooterSubText>
                     ini text uhuy
                 </FooterSubText>
-            </FooterSubscription>            
+            </FooterSubscription>
+            <FooterLinksContainer>
+                <FooterLinksWrapper>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>About Us</FooterLinkTitle>
+                        <FooterLink to='/sign-up'>How it works</FooterLink>
+                        <FooterLink to='/'>Testimonials</FooterLink>
+                        <FooterLink to='/'>Careers</FooterLink>
+                        <FooterLink to='/'>Investors</FooterLink>
+                        <FooterLink to='/'>Terms of Service</FooterLink>
+                    </FooterLinkItems>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                        <FooterLink to='/'>Contact</FooterLink>
+                        <FooterLink to='/'>Support</FooterLink>
+                        <FooterLink to='/'>Destinations</FooterLink>
+                        <FooterLink to='/'>Sponsorships</FooterLink>
+                    </FooterLinkItems>
+                    </FooterLinksWrapper>
+                    <FooterLinksWrapper>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>Videos</FooterLinkTitle>
+                        <FooterLink to='/'>Submit Video</FooterLink>
+                        <FooterLink to='/'>Ambassadors</FooterLink>
+                        <FooterLink to='/'>Agency</FooterLink>
+                        <FooterLink to='/'>Influencer</FooterLink>
+                    </FooterLinkItems>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>Social Media</FooterLinkTitle>
+                        <FooterLink to='/'>Instagram</FooterLink>
+                        <FooterLink to='/'>Facebook</FooterLink>
+                        <FooterLink to='/'>Youtube</FooterLink>
+                        <FooterLink to='/'>Twitter</FooterLink>
+                    </FooterLinkItems>
+                </FooterLinksWrapper>
+            </FooterLinksContainer>            
         </FooterContainer>
     );
 }
--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom';
 
 export const FooterContainer = styled.div`
-  background-color: #101522;
+  background-color: #fff;
   padding: 4rem 0 2rem 0;
   display: flex;
   flex-direction: column;
@@ -17,7 +18,7 @@ export const FooterSubscription = styled.section`
   text-align: center;
   margin-bottom: 24px;
   padding: 24px;
-  color: #fff;
+  color: #101522;
 `;
 
 export const FooterSubHeading = styled.p`
@@ -30,4 +31,51 @@ export const FooterSubHeading = styled.p`
 export const FooterSubText = styled.p`
   margin-bottom: 24px;
   font-size: 20px;
+`;
+
+export const FooterLinksContainer = styled.div`
+  width: 100%;
+  max-width: 1000px;
+  display: flex;
+  justify-content: center;
+  @media screen and (max-width: 820px) {
+    padding-top: 32px;
+  }
+`;
+
+export const FooterLinksWrapper = styled.div`
+  display: flex;
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+  }
+`;
+
+export const FooterLinkItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 16px;
+  text-align: left;
+  width: 160px;
+  box-sizing: border-box;
+  color: #101522;
+  @media screen and (max-width: 420px) {
+    margin: 0;
+    padding: 10px;
+    width: 100%;
+  }
+`;
+
+export const FooterLinkTitle = styled.h2`
+  margin-bottom: 16px;
+`;
+
+export const FooterLink = styled(Link)`
+  color: #101522;
+  text-decoration: none;
+  margin-bottom: 0.5rem;
+  &:hover {
+    color: #0467fb;
+    transition: 0.3s ease-out;
+  }
 `;
