59d92ba7cf5e6fdedbd52e2ddf7b264cea0840f0
rickylukman <rickylukman0606@gmail.com>
2021-01-28 21:16:50 +0700

    sekedar update

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -37,11 +37,6 @@ const InfoSection = ({
                             <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
                             <Heading lightText={lightText}>{headline}</Heading>
                             <Subtitle lightTextDesc={lightTextDesc}>{description}</Subtitle>
-                            {/* <Link to='/sign-up'>
-                                <Button big fontBig primary={primary}>
-                                    {buttonLabel}
-                                </Button>
-                            </Link> */}
                         </TextWrapper>
                     </InfoColumn>
                     <InfoColumn>
